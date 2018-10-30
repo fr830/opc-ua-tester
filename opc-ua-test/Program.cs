13b7a8f6841a7cd23144f32c0d2c16c429f9ba8d
jgn <jgn@au2mate.dk>
2018-10-30 23:27:49 +0100

    Initial commit

---
+++ opc-ua-test/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpcLabs.EasyOpc.UA;
+using OpcLabs.EasyOpc.UA.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace opc_ua_test {
+    class Program {
+        static void Main(string[] args) {
+
+            if (args.Length < 2) {
+                Console.WriteLine("Not enough args :(");
+                return;
+            }
+
+            UAEndpointDescriptor server = $"opc.tcp://{args[0]}";
+
+            // How to keep quotes in user input string? :thinking:
+            UANodeDescriptor node = args[1];
+
+            EasyUAClient client = new EasyUAClient();
+
+            UAAttributeData data;
+
+            while (true) {
+
+                data = client.Read(server, node);
+
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine($"Read timestamp: {data.SourceTimestampLocal}");
+                Console.WriteLine($"Read status: {data.StatusCode}");
+                Console.WriteLine($"Read value type is: {data.ValueType}");
+                Console.WriteLine($"Read value is: {data.Value}");
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
